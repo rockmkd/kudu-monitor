c13303c5b68ccce27d698b6bfaa6a9f4b5c14bc0
rockmkd <rockmkd@gmail.com>
2019-01-10 23:26:25 +0900

    add UI component

--- frontend/src/app/metric.service.ts
+++ frontend/src/app/metric.service.ts
@@ -6,24 +6,23 @@ import { switchMap } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class MetricService {
 
-  private readonly API_URL = "localhost";
+  private readonly API_URL = 'localhost';
 
   constructor(private httpClient: HttpClient) { }
 
   public allMetric(): Observable<any> {
     return this.getHostList().pipe(
-      switchMap( (hosts:string[]) => {
-        console.log(hosts);
+      switchMap((hosts: string[]) => {
         return this.getMetrics(hosts[0]);
       })
-    )
+    );
   }
 
   private getHostList(): Observable<string[]> {
     return this.httpClient.get<string[]>(`${this.API_URL}/hosts`);
   }
 
-  private getMetrics(hostname:string): Observable<object> {
+  private getMetrics(hostname: string): Observable<object> {
     return this.httpClient.get<string[]>(`${this.API_URL}/metric?host=${hostname}`);
   }
 
